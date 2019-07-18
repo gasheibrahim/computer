8f18237d73fb671d29f0b5adf7710a7a7a893845
unknown <ibrahimgashema154@gmail.com>
2019-07-18 18:07:48 +0200

    project

---
+++ index.php
@@ -0,0 +1,203 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Basic stylesheet -->
+    <link rel="stylesheet" href="owl-carousel/owl.carousel.css">
+ 
+ <!-- Default Theme -->
+    <link rel="stylesheet" href="owl-carousel/owl.theme.css">
+  
+ <!-- You can use latest version of jQuery  -->
+    <script src="jquery-1.9.1.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="assets/owl-carousel/owl.carousel.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.mousewheel-3.0.6.pack.js"></script>
+    <script src="js/paralax.js"></script>
+    <script src="js/jquery.smooth-scroll.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/main.js"></script>
+  
+ <!-- Include js plugin -->
+    <script src="assets/owl-carousel/owl.carousel.js"></script>
+    <title>Ms Computers Rwanda</title>
+</head>
+<body>
+    <div class="container">
+      <nav class="navbar navbar-expand-lg fixed-top"  style="background-color:white;">
+      <a class="navbar-brand" href="#"><img src="images/logo.jpg" width="180" height="140" alt="logo"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#" id="active">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About US</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Products</a>
+          </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Partners</a>
+          </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Clients</a>
+          </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+<div class="container">
+<div class="col-md-12">
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="images/10.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="images/10.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="images/10.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
+</div><br><br><br>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-8">
+      <h3>Welcome To Ms Computers Center</h3>
+      <p>ICN Computer Center is the leading Systems Integration and IT Solutions Company in Rwanda and Burundi.
+       For the last decade, we have successfully served the technology needs of more than 2500 customers from diverse industries and locations.
+       With an achievement of HP as the best in Rwanda HP award of the Year 2017-2018 in Nairobi Kenya.</p>
+    </div>
+    <div class="col-sm-4">
+      <img src="images/5.jpg" alt="">
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-8">
+      <p>ICN Computer Center is the leading Systems Integration and IT Solutions Company in Rwanda and Burundi.
+       For the last decade, we have successfully served the technology needs of more than 2500 customers from diverse industries and locations.
+       With an achievement of HP as the best in Rwanda HP award of the Year 2017-2018 in Nairobi Kenya.</p>
+    </div>
+    <div class="col-sm-4">
+      <img src="images/5.jpg" alt="">
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-8">
+      <p>ICN Computer Center is the leading Systems Integration and IT Solutions Company in Rwanda and Burundi.
+       For the last decade, we have successfully served the technology needs of more than 2500 customers from diverse industries and locations.
+       With an achievement of HP as the best in Rwanda HP award of the Year 2017-2018 in Nairobi Kenya.</p>
+    </div>
+    <div class="col-sm-4">
+      <img src="images/5.jpg" alt="">
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-8">
+      <p>ICN Computer Center is the leading Systems Integration and IT Solutions Company in Rwanda and Burundi.
+       For the last decade, we have successfully served the technology needs of more than 2500 customers from diverse industries and locations.
+       With an achievement of HP as the best in Rwanda HP award of the Year 2017-2018 in Nairobi Kenya.</p>
+    </div>
+    <div class="col-sm-4">
+      <img src="images/5.jpg" alt="">
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-8">
+      <h3>Welcome To Ms Computers Center</h3>
+      <p>ICN Computer Center is the leading Systems Integration and IT Solutions Company in Rwanda and Burundi.
+       For the last decade, we have successfully served the technology needs of more than 2500 customers from diverse industries and locations.
+       With an achievement of HP as the best in Rwanda HP award of the Year 2017-2018 in Nairobi Kenya.</p>
+    </div>
+    <div class="col-sm-4">
+      <img src="images/5.jpg" alt="">
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-8">
+        <h1>Parterners</h1>
+    </div>
+    <div class="col-sm-8">
+    <div id="owl-demo" class="owl-carousel owl-theme">
+          
+    <div class="item"><img src="images/1.jpg" alt="Owl Image"</div>
+    <div class="item"><img src="images/2.jpg" alt="Owl Image"</div>
+    <div class="item"><img src="images/3.jpg" alt="Owl Image"</div>
+    <div class="item"><img src="images/4.jpg" alt="Owl Image"</div>
+    <div class="item"><img src="images/5.jpg" alt="Owl Image"</div>
+    <div class="item"><img src="images/6.jpg" alt="Owl Image"</div>
+    <div class="item"><img src="images/7.jpg" alt="Owl Image"</div>
+    <div class="item"><img src="images/8.jpg" alt="Owl Image"</div>
+ 
+</div>
+    </div>
+  </div>
+</div>
+
+<script>
+$(document).ready(function() {
+ 
+ $("#owl-demo").owlCarousel({
+
+     autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+     items : 4,
+     itemsDesktop : [1199,3],
+     itemsDesktopSmall : [979,3]
+
+ });
+
+});
+$(document).ready(function() {
+$("#owl-demo").owlCarousel({
+navigation : true, // Show next and prev buttons
+slideSpeed : 300,
+paginationSpeed : 400,
+items : 1,
+itemsDesktop : false,
+itemsDesktopSmall : false,
+itemsTablet: false,
+itemsMobile : false
+});
+});
+
+</script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
+</html>
