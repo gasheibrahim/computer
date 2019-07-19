dcf436dd5df73e590a03f1220e95ce3f858be795
unknown <ibrahimgashema154@gmail.com>
2019-07-19 23:48:53 +0200

    mscomputer

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <html lang="en">
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/lightbox.min.css">
+    <script type="text/javascript" src="js/lightbox-plus-jquery.min.js"></script>
+    <link rel="stylesheet" href="dist/css/bootstrap.min.css">
     <!-- Basic stylesheet -->
     <link rel="stylesheet" href="owl-carousel/owl.carousel.css">
  
@@ -25,35 +28,42 @@
     <script src="js/main.js"></script>
   
  <!-- Include js plugin -->
-    <script src="assets/owl-carousel/owl.carousel.js"></script>
     <title>Ms Computers Rwanda</title>
 </head>
 <body>
+  <div class="container-flued">
+    <div class="row">
+      <div class="col-sm-12" style="background-color: darkred;color: white;">
+        <h3 style="line-height: 25px;float: right;">Phone Number: 078xxxxxxxxx</h3>
+      </div>
+    </div>
+  </div>
     <div class="container">
-      <nav class="navbar navbar-expand-lg fixed-top"  style="background-color:white;">
+    <div class="row">
+        <nav class="navbar navbar-expand-lg">
       <a class="navbar-brand" href="#"><img src="images/logo.jpg" width="180" height="140" alt="logo"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarText">
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#" id="active">Home</a>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php" id="active">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">About US</a>
+            <a class="nav-link" href="about.php">About US</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Products</a>
+            <a class="nav-link" href="products.php">Products</a>
           </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Services</a>
+            <a class="nav-link" href="services.php">Services</a>
           </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Partners</a>
+            <a class="nav-link" href="partners.php">Partners</a>
           </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Clients</a>
+            <a class="nav-link" href="client.php">Clients</a>
           </li>
         <li class="nav-item">
             <a class="nav-link" href="#">Contact Us</a>
@@ -61,7 +71,8 @@
         </ul>
       </div>
     </nav>
-  </div>
+    </div>
+</div>
 <div class="container">
 <div class="col-md-12">
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
@@ -149,55 +160,10 @@
   </div>
   <div class="row">
     <div class="col-sm-8">
-        <h1>Parterners</h1>
-    </div>
-    <div class="col-sm-8">
-    <div id="owl-demo" class="owl-carousel owl-theme">
-          
-    <div class="item"><img src="images/1.jpg" alt="Owl Image"</div>
-    <div class="item"><img src="images/2.jpg" alt="Owl Image"</div>
-    <div class="item"><img src="images/3.jpg" alt="Owl Image"</div>
-    <div class="item"><img src="images/4.jpg" alt="Owl Image"</div>
-    <div class="item"><img src="images/5.jpg" alt="Owl Image"</div>
-    <div class="item"><img src="images/6.jpg" alt="Owl Image"</div>
-    <div class="item"><img src="images/7.jpg" alt="Owl Image"</div>
-    <div class="item"><img src="images/8.jpg" alt="Owl Image"</div>
- 
-</div>
+        <h3>Parterners</h3>
     </div>
   </div>
 </div>
 
-<script>
-$(document).ready(function() {
- 
- $("#owl-demo").owlCarousel({
-
-     autoPlay: 3000, //Set AutoPlay to 3 seconds
-
-     items : 4,
-     itemsDesktop : [1199,3],
-     itemsDesktopSmall : [979,3]
-
- });
-
-});
-$(document).ready(function() {
-$("#owl-demo").owlCarousel({
-navigation : true, // Show next and prev buttons
-slideSpeed : 300,
-paginationSpeed : 400,
-items : 1,
-itemsDesktop : false,
-itemsDesktopSmall : false,
-itemsTablet: false,
-itemsMobile : false
-});
-});
-
-</script>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    </body>
+</body>
 </html>
